15ede0c6d57ec042acff8b9729cea482846f4931
Ricky <hollowtaylor@gmail.com>
2021-02-10 12:00:43 +0000

    character namespace

--- HeadlessChickensUnity/Assets/_Project/Scripts/Character/CharacterBase.cs
+++ HeadlessChickensUnity/Assets/_Project/Scripts/Character/CharacterBase.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class CharacterBase : MonoBehaviour
+namespace _Project.Scripts.Character
 {
-    private CharacterController _controller;
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterBase : MonoBehaviour
+    {
+        private CharacterController _controller;
 
 
-    private void Awake()
-    {
-        _controller = GetComponent<CharacterController>();
-    }
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
 
-    void Start()
-    {
-        
-    }
-    
-    
-    void Update()
-    {
-        
+        void Start()
+        {
+        }
+
+
+        void Update()
+        {
+
+        }
+
     }
 }
--- HeadlessChickensUnity/Assets/_Project/Scripts/Character/CharacterController.cs
+++ HeadlessChickensUnity/Assets/_Project/Scripts/Character/CharacterController.cs
@@ -1,67 +1,66 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
-public class CharacterController : MonoBehaviour
+namespace _Project.Scripts.Character
 {
-    private InputControls _controls;
-
-    private Vector3 _newPosition;
-    private Vector2 _movDirection;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private Quaternion _newRotation;
-    [SerializeField] private float rotationTime;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float moveTime;
-
-    private void Awake()
+    public class CharacterController : MonoBehaviour
     {
-        _controls = new InputControls();
-        _controls.Enable();
+        private InputControls _controls;
 
-        _controls.Player.Move.performed += MoveStarted;
-        _controls.Player.Move.canceled += MoveCanceled;
-    }
+        private Vector3 _newPosition;
+        private Vector2 _movDirection;
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private Quaternion _newRotation;
+        [SerializeField] private float rotationTime;
+        [SerializeField] private float rotationSpeed;
+        [SerializeField] private float moveTime;
 
-    private void MoveStarted(InputAction.CallbackContext obj)
-    {
-        _movDirection = obj.ReadValue<Vector2>(); 
-        
-    }
-
-    private void MoveCanceled(InputAction.CallbackContext obj)
-    {
-        _movDirection = Vector2.zero;
-    }
-
-    private void Start()
-    {
-        _newRotation = transform.rotation;
-        _newPosition = transform.position;
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-        if (_movDirection.x != 0 && _movDirection.y != 0)
+        private void Awake()
         {
-            _newPosition += transform.forward * (_movDirection.y * moveSpeed);
-            _newRotation *= Quaternion.Euler(Vector3.up * _movDirection.x);
+            _controls = new InputControls();
+            _controls.Enable();
+
+            _controls.Player.Move.performed += MoveStarted;
+            _controls.Player.Move.canceled += MoveCanceled;
         }
-        if (_movDirection.y != 0)
-            _newPosition += transform.forward * (_movDirection.y * moveSpeed);
-        if(_movDirection.x != 0)
-            _newRotation *= Quaternion.Euler(Vector3.up * (_movDirection.x * rotationSpeed));
+
+        private void MoveStarted(InputAction.CallbackContext obj)
+        {
+            _movDirection = obj.ReadValue<Vector2>(); 
         
-        transform.position = Vector3.Lerp(transform.position, _newPosition, Time.deltaTime * moveTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation, _newRotation, Time.deltaTime * rotationTime);
+        }
+
+        private void MoveCanceled(InputAction.CallbackContext obj)
+        {
+            _movDirection = Vector2.zero;
+        }
+
+        private void Start()
+        {
+            _newRotation = transform.rotation;
+            _newPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (_movDirection.x != 0 && _movDirection.y != 0)
+            {
+                _newPosition += transform.forward * (_movDirection.y * moveSpeed);
+                _newRotation *= Quaternion.Euler(Vector3.up * _movDirection.x);
+            }
+            if (_movDirection.y != 0)
+                _newPosition += transform.forward * (_movDirection.y * moveSpeed);
+            if(_movDirection.x != 0)
+                _newRotation *= Quaternion.Euler(Vector3.up * (_movDirection.x * rotationSpeed));
+        
+            transform.position = Vector3.Lerp(transform.position, _newPosition, Time.deltaTime * moveTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, _newRotation, Time.deltaTime * rotationTime);
             
+        }
     }
 }
