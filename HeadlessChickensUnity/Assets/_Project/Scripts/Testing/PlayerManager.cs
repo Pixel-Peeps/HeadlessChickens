b96b3693e4d173d533ad28bf783d61456d3acf88
bekah <caffeine@hotmail.co.uk>
2021-02-16 14:29:24 +0000

    tidying up, next: working nicknames

--- HeadlessChickensUnity/Assets/_Project/Scripts/Testing/PlayerManager.cs
+++ HeadlessChickensUnity/Assets/_Project/Scripts/Testing/PlayerManager.cs
@@ -25,8 +25,7 @@ namespace com.pixelpeeps.headlesschickens
         
         [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
         public static GameObject LocalPlayerInstance;
-
-        // Start is called before the first frame update
+        
         void Awake()
         {
             // #Important
@@ -37,8 +36,7 @@ namespace com.pixelpeeps.headlesschickens
             }
 
             charController = gameObject.GetComponent<AlternativeCharacterInput>();
-            _camera = Camera.main;
-           // rB = gameObject.GetComponent<Rigidbody>();
+            
             // #Critical
             // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
             DontDestroyOnLoad(this.gameObject);
@@ -50,26 +48,15 @@ namespace com.pixelpeeps.headlesschickens
             // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
             UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
 #endif
-
-            //Camera _camera = Camera.main;
-            //Camera _camera = gameObject.GetComponent<Camera>();
-
-
-            //if (_camera != null)
-            //{
+            
             if (photonView.IsMine)
             {
-                    
-                //_camera.gameObject.SetActive(true);
+                //inst a rigidbody, this may not be necessary but otherwise maybe later problems
                 Rigidbody rB = gameObject.AddComponent<Rigidbody>();
                 rB.useGravity = false;
                 charController.enabled = true;
                 vCam.SetActive(true);
 
-
-                //_camera.gameObject.transform.SetParent(this.transform);
-                //_camera.gameObject.transform.position = new Vector3(-0.43f,1.8f,-4.4f);
-                //_camera.gameObject.transform.rotation = new Quaternion(14.6f, 0, 0,0);
                 photonView.RPC("RPC_ChangeHealth", RpcTarget.All);
             }
         }
@@ -84,15 +71,16 @@ namespace com.pixelpeeps.headlesschickens
             
             if (photonView.IsMine)
             {
+                //what does this do?
                 //this.ProcessInputs();
+                
                 if (Health <= 0f)
                 {
                     Health = 30;
+                    
                     //GameManager.Instance.LeaveRoom();
                 }
             }
-
-           // healthDisplay.text = Health.ToString();
         }
 
 
@@ -111,18 +99,16 @@ namespace com.pixelpeeps.headlesschickens
             if (stream.IsWriting)
             {
                 // We own this player: send the others our data
-                //stream.SendNext(IsFiring);
                 Debug.Log("running on local (health)");
+                stream.SendNext(Health);
                 healthDisplay.text = this.Health.ToString("0");
-              stream.SendNext(Health);
             }
             else
             {
                 // Network player, receive data
-                //this.IsFiring = (bool)stream.ReceiveNext();
                 Debug.Log("running on remote (health)");
-              this.Health = (float)stream.ReceiveNext();
-              healthDisplay.text = this.Health.ToString("0");
+                this.Health = (float)stream.ReceiveNext();
+                healthDisplay.text = this.Health.ToString("0");
             }
             
         }
@@ -148,12 +134,7 @@ namespace com.pixelpeeps.headlesschickens
                 transform.position = new Vector3(0f, 5f, 0f);
             }
         }
-        /// <summary>
-        /// MonoBehaviour method called when the Collider 'other' enters the trigger.
-        /// Affect Health of the Player if the collider is a beam
-        /// Note: when jumping and firing at the same, you'll find that the player's own beam intersects with itself
-        /// One could move the collider further away to prevent this or check if the beam belongs to the player.
-        /// </summary>
+        
         void OnTriggerEnter(Collider other)
         {
             Debug.Log("Trigger enter");
@@ -161,21 +142,20 @@ namespace com.pixelpeeps.headlesschickens
             {
                 return;
             }
-            // We are only interested in Beamers
-            // we should be using tags but for the sake of distribution, let's simply check by name.
             if (other.gameObject.CompareTag("Player"))
             {
                 Health -= 1f;
                 Debug.Log(this.name + Health);
             }
+            
+            //call the RPC, this one does nothing
             photonView.RPC("RPC_ChangeHealth", RpcTarget.All);
         }
 
         [PunRPC]
         void RPC_ChangeHealth()
         {
-            //this.Health -= 1;
-            //this.healthDisplay.text = this.Health.ToString("0");
+            //don't need this for health, just leaving as an RPC template
         }
         
 #if UNITY_5_4_OR_NEWER
