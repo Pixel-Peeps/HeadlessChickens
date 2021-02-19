fd8f90ef38179b9b7642e4425990214c8c9058ee
Daniel Gould <danfgould@gmail.com>
2021-02-19 10:35:14 +0000

    Take 3

--- HeadlessChickensUnity/Assets/_Project/Scripts/Interactables/Interact Actions/HidingSpot.cs
+++ HeadlessChickensUnity/Assets/_Project/Scripts/Interactables/Interact Actions/HidingSpot.cs
@@ -14,7 +14,7 @@ public class HidingSpot : MonoBehaviourPunCallbacks, IInteractable
             if (!inUse)
             {
                 Debug.Log("Interact() method called, inUse false");
-                character.currentHidingSpot.position = transform.position;
+                character.currentHidingSpot = transform;
                 character.HidingInteraction();
                 inUse = true;
             }
